32299b624f221adfb6e9dcac78167fc5265627a1
Ryan Scott Lewis <ryanscottlewis@gmail.com>
2022-02-13 22:30:08 -0500

    ColorSwatch: Rename parameter in paintComponent

--- src/us/rynet/akaifire/ColorSwatch.java
+++ src/us/rynet/akaifire/ColorSwatch.java
@@ -19,19 +19,20 @@ public class ColorSwatch extends AbstractButton {
   }
 
   @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
+  protected void paintComponent(Graphics graphics) {
+    super.paintComponent(graphics);
 
     Dimension originalSize = super.getPreferredSize();
     int       gap          = (int)(originalSize.height * 0.2);
     int       x            = originalSize.width + gap;
     int       y            = gap;
 
-    Color color = new Color(pad.getRed() * 2, pad.getGreen() * 2, pad.getBlue() * 2);
-    g.setColor(color);
+    // Color color = new Color(pad.getRed() * 2, pad.getGreen() * 2, pad.getBlue() * 2);
+    Color color = new Color(pad.getRed(), pad.getGreen(), pad.getBlue());
+    graphics.setColor(color);
 
     Dimension size = getPreferredSize();
-    g.drawRect(x, y, size.width, size.height);
+    graphics.drawRect(x, y, size.width, size.height);
   }
 
   @Override
